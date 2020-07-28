afc913d63145bfc0994c78fefefbcdbed66b22ea
LOKU SINGHAMKUTTI K Dushantha Maduranga <dmlokusing@starhub.com>
2020-07-29 01:55:20 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dataSource from './data/dataStore.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      district:"",
+      pci:"",
+      places:[],
+      };
+  }
+  myChangeHandler = () => {
+    let filteredArr = [];
+    filteredArr = dataSource.filter(a => a.District.toUpperCase() == this.state.district.toUpperCase() && a.physicalCellID == this.state.pci);
+    console.log(filteredArr);
+    this.setState({places:filteredArr});
+  }
+
+  handleDistrict = (e) => {
+    this.setState({district:e.target.value});
+  }
+
+  handlePCI = (e) => {
+    this.setState({pci:e.target.value});
+  }
+
+  renderTableData() {
+    return this.state.places.map((place, index) => {
+       const { localCellIdentity, downlinkEARFCN, cellName, downlinkBandwidth } = place //destructuring
+       return (
+          <tr key={localCellIdentity}>
+             <td><strong>local Cell Identity</strong> : {localCellIdentity}</td>
+             <td><strong>Downlink EARFCN</strong> : {downlinkEARFCN}</td>
+             <td><strong>Cell Name</strong> : {cellName}</td>
+             <td><strong>Downlink bandwidth</strong> : {downlinkBandwidth}</td>
+          </tr>
+       )
+    })
+ }
+
+  render() {
+    return (
+      <div style={{margin:20}}>
+      <h1>Search Data</h1>
+      <p>Enter District:</p>
+      <input
+        type='text' value={this.state.district} onChange={this.handleDistrict}
+      />
+       <p>Enter PCI:</p>
+      <input
+        type='text' value={this.state.pci} onChange={this.handlePCI}
+      />
+      <button
+      type="button"
+      onClick={this.myChangeHandler}>Search</button>
+      
+      <table id='places'>
+               <tbody>
+                  {this.renderTableData()}
+               </tbody>
+            </table>
+
+      </div>
+     
+    
+    );
+  }
 }
 
 export default App;
